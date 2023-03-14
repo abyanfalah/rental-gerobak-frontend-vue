105f265176fed00ff44099b385bb96c54bba47ba
Abyan Falah <abyanf410@gmail.com>
2023-03-14 20:54:34 +0700

    Update Rent.vue: included full customer data

--- src/views/Rent.vue
+++ src/views/Rent.vue
@@ -5,11 +5,13 @@ import rentService from "../service/modules/rentService";
 import dateTimeService from "../service/modules/dateTimeService";
 import { useIndexStore } from "../stores";
 import ModalRentPay from "../components/ModalRentPay.vue";
+import customerService from "../service/modules/customerService";
 
 const getDateTime = dateTimeService.getReadableDateTime
 const indexStore = useIndexStore()
 
 const rentList = ref();
+const customerList = ref()
 const error = ref(false)
 const choosenRent = ref({})
 
@@ -19,7 +21,6 @@ async function getRent() {
     const response = await rentService.getAll();
 		error.value = false
 		rentList.value = response.data.data;
-		console.log(rentList.value)
   } catch (err) {
 		error.value = true
     console.error(err);
@@ -33,10 +34,15 @@ function showRent(rent) {
 	indexStore.choosenRent = rent
 }
 
-console.log(indexStore.choosenRent)
+const searchQuery = ref()
+function filter() {
+	console.log(`looking for ${searchQuery.value}`)
+}
 
-onMounted(() => {
+onMounted( async () => {
 	getRent();
+
+	customerList.value = await customerService.getAll()
 });
 </script>
 
@@ -44,7 +50,7 @@ onMounted(() => {
 		<div>
 		<div class="row">
 			<div class="col">
-				<h1>Tabel rental</h1>
+				<h1>Rental</h1>
 			</div>
 			<div class="col text-end">
 				<RouterLink class="btn btn-success shadow-sm" to="/rent/new">
@@ -59,6 +65,30 @@ onMounted(() => {
 			<!-- rent table column -->
 			<div class="col-md-7">
 				<div class="card">
+					<div class="card-header d-flex justify-content-between align-items-center">
+						<span>Tabel rental</span>
+
+						<div>
+							<!-- search bar -->
+							<div class="input-group">
+								<input
+								type="text" 
+								list="customers"
+								v-model="searchQuery"
+								@keyup="filter()"
+								class="form-control" 
+								placeholder="Ketik disini untuk mencari">
+								<datalist id="customers">
+									<option v-for="customer in customerList" :value="customer.name">{{ customer.name }}</option>
+								</datalist>
+
+								<button class="btn" :class="{'btn-danger' : searchQuery}" type="button" @click="clearFilter">
+										<i class="bi-x-lg"></i>
+								</button>
+							</div>
+						</div>
+					</div>
+
 					<div class="card-body">
 						<p class="text-center muted" v-if="error">Error: cannot fetch data.</p>
 						<table v-else class="table table-sm table-hover">
@@ -188,5 +218,5 @@ onMounted(() => {
 
 	<!-- <div v-if="choosenRent.id">{{ choosenRent }}</div> -->
 
-	<ModalRentPay />
+	<ModalRentPay @payment-success="getRent()" />
 </template>

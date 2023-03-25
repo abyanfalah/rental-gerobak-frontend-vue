73c0069f5f7ca19f69b9a56aa3e6ff0cd1f0db05
Abyan Falah <abyanf410@gmail.com>
2023-03-25 16:41:07 +0700

    Add ModalUserEditConfirmation.vue

---
+++ src/components/ModalUserEditConfirmation.vue
@@ -0,0 +1,26 @@
+<script setup>
+import router from "../router";
+import authService from "../service/modules/authService";
+import { useAuthStore } from "../stores/auth";
+
+const emit = defineEmits(['userEditConfirm'])
+</script>
+
+<template>
+	<div class="modal fade" data-bs-backdrop="static" id="modalUserEditConfirmation" >
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<div class="modal-header bg-primary text-white">
+					<h5 class="modal-title">Konfirmasi</h5>
+				</div>
+				<div class="modal-body">
+					Simpan perubahan?
+				</div>
+				<div class="modal-footer border-0">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+					<button type="button" class="btn btn-success" data-bs-dismiss="modal" @click="emit('userEditConfirm')">Ya, simpan</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
